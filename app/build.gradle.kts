e92432cca96631989c71e240d224a60136a360e5
Rick Kimotho <rickkimotho.dev@gmail.com>
2025-11-24 14:28:57 -0600

    chore: Set Kotlin JVM target to 17
    This commit configures the `jvmTarget` for Kotlin to version 17, aligning it with the Java target compatibility version. The app wasn't running with the previous commit since this was missing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     kotlin {
         compilerOptions {
             freeCompilerArgs.addAll(
